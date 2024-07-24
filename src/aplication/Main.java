38e17054efcaed25b3bce7256792ccdddeffde86
Dailton Torres <dailtontorres@outlook.com>
2024-07-24 09:11:16 -0300

    Criando a classe Position

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,9 +1,12 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Agora vai parte 2");
+		Position position = new Position(5, 3);
+		System.out.println(position);
 
 	}
 
