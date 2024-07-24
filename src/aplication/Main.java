856d5b908b8b65cab6126a4005984cd45a15489c
Dailton Torres <dailtontorres@outlook.com>
2024-07-24 08:17:57 -0300

    Fazendo a conexao

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -3,7 +3,7 @@ package aplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		System.out.println("o projeto esta dando errado");
 
 	}
 
