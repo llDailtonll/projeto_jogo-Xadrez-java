41e3244e96139988b7dcc80ddb45d683b2b8445d
Dailton Torres <dailtontorres@outlook.com>
2024-07-24 08:23:38 -0300

    agora vai

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -3,7 +3,7 @@ package aplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("o projeto esta dando errado parte 2");
+		System.out.println("Agora vai");
 
 	}
 
